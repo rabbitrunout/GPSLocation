5ffc20f66485f66429ed5b9252ce67d0a5ea2a79
Irina Saf <irina.safronova0801@gmail.com>
2025-12-08 10:07:53 -0500

    Merge remote-tracking branch 'origin/main'

--- app/src/main/java/com/example/gpslocation/MainActivity.kt
+++ app/src/main/java/com/example/gpslocation/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.example.gpslocation
-
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var latlngText: TextView
+    private lateinit var addressText: TextView
+    private val LOCATION_PERMISSION_REQUEST= 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        latlngText = findViewById(R.id.latlngText)
+        addressText = findViewById(R.id.addressText)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
